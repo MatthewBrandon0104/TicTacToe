4b0e55760a8bfc1a83c78e280c6d98cf60ff5f72
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-08-01 20:56:30 -0700

    made player selection

--- ruby.rb
+++ ruby.rb
@@ -8,9 +8,22 @@ class User
         @@counter += 1
     end
     
-    def make_selection
-        p "Make a selection"
-        return gets.chomp
+    def selection
+        i = 0
+        while i < 1
+            p "#{self} make a selection..."
+            temp = gets.chomp.downcase
+            if temp.length != 2
+                p "Error, input must be 2 characters..."
+            elsif (["a","b","c"].include? (temp[0])) == false
+                p "Error, please select either row a, b, or c..."
+            elsif (["1","2","3"].include? (temp[1])) == false
+                p "Error, please select a column as 1, 2, or 3..."
+            else
+                p "input successful..."
+                return temp
+            end
+        end
     end
     
 end
